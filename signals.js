3dba74bc211baea3e5b94edd0fd671011be1d6ac
udaykanth R <udaykanth.rapeti@gmail.com>
2014-10-05 12:29:28 +0530

    fixed issues

--- signals.js
+++ signals.js
@@ -5,21 +5,22 @@ var Gpio = require('onoff').Gpio,
    _baseTime = 100000, //micro seconds
    second = _baseTime * 10,
    sensorData = true,
-   MAX_TIME = 10;
+   MAX_TIME = 10,
+   MIN_TIME = 5;
 var lanes = [
-	{time: MAX_TIME, led: {red: 17, green: 18}, freetime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */},
-	{time: MAX_TIME, led: {red: 17, green: 18}, freetime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */},
-	{time: MAX_TIME, led: {red: 17, green: 18}, freetime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */},
-	{time: MAX_TIME, led: {red: 17, green: 18}, freetime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */}
+	{time: MAX_TIME, led: {red: new Gpio(17, 'out'), green: new Gpio(23, 'out')}, freeTime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */},
+	{time: MAX_TIME, led: {red: new Gpio(17, 'out'), green: new Gpio(17, 'out')}, freeTime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */},
+	{time: MAX_TIME, led: {red: new Gpio(17, 'out'), green: new Gpio(17, 'out')}, freeTime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */},
+	{time: MAX_TIME, led: {red: new Gpio(17, 'out'), green: new Gpio(17, 'out')}, freeTime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */}
 	];
 console.log("Initialized the ports sucessfully..."); 
 var signals = {
    on: function(led) {
-      //led.writeSync(1);
+      led.writeSync(1);
 	  
    },
    off: function(led) {
-      //led.writeSync(0);
+      led.writeSync(0);
    }
 };
 
@@ -63,12 +64,13 @@ while (true) {
 	lane.freeTime += sensorCount;
 	if (lane.ftItr >= 4) {
 	console.log("time :" + lane.freeTime + " Itr: " + lane.ftItr);
-		var avgTime = (Math.floor(lane.freeTime/(lane.ftItr + 1)));
+		var avgTime = MAX_TIME - (Math.floor(lane.freeTime/(lane.ftItr + 1)));
 		console.log("Average time Set for Lane: " + (num+1) + " Is : " + avgTime + " Seconds.");		
-		avgTime += 5;
+		if (isNaN(avgTime)) { avgTime = MIN_TIME;}
+		avgTime += 5;		
 		if (avgTime>= MAX_TIME) {avgTime = MAX_TIME;}
-		if (isNaN(avgTime)) { avgTime = 10;}
-		if (avgTime < 10 ) {avgTime = 10;}
+		
+		if (avgTime < MIN_TIME ) {avgTime = MIN_TIME;}
 		lane.time = avgTime;
 		lane.ftItr = 0;
 		lane.freeTime = 0;
