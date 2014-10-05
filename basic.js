025ad792fe5d173589eaaf5df5a7ffe9e4ea2458
urapetix <udaykanthx.rapeti@intel.com>
2014-10-05 05:52:29 +0530

    basic js

--- basic.js
+++ basic.js
@@ -1,1 +1,12 @@
 console.log("This is a nodeJS Application...");
+console.log("Initialized the ports sucessfully..."); 
+var Gpio = require('onoff').Gpio,
+   led = new Gpio(17, 'out'),
+   sleep = require('sleep'),
+   _baseTime = 128000, //micro seconds
+   sleepTime = _baseTime, 
+   btwCodes = _baseTime * 2,
+   btwLetters = _baseTime * 4,
+   btwWords = _baseTime * 8;
+ 
+console.log("Initialized the ports sucessfully..."); 
