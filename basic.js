feb1da37bfebeb02f1165a060d69e191db9cc944
urapetix <udaykanthx.rapeti@intel.com>
2014-10-05 06:43:29 +0530

    basic js

--- basic.js
+++ basic.js
@@ -7,11 +7,21 @@ var Gpio = require('onoff').Gpio,
    sleepTime = _baseTime,
    btwWords = _baseTime * 8;;
 console.log("Initialized the ports sucessfully..."); 
+var signals = {
 
+   blink: function() {
+      led.writeSync(1);
+   },
+   reset: function() {
+      led.writeSync(0);
+   }
+}
 console.log("Executing the ports..");    
 for (var i=0; i<10; i++) {
 	console.log("setting port17 to ON state");
+	signals.bink();
 	sleep.usleep(btwWords);
+	signals.reset();
 	console.log("setting port17 to OFF state");
 }   
  
