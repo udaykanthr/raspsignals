115cae7694fa96716308b22fc000e0c49180ec83
udaykanth R <udaykanth.rapeti@gmail.com>
2014-10-05 11:06:30 +0530

    fixed issues

--- signals.js
+++ signals.js
@@ -4,12 +4,13 @@ var Gpio = require('onoff').Gpio,
    sleep = require('sleep'),
    _baseTime = 100000, //micro seconds
    second = _baseTime * 10,
-   sensorData = true;
+   sensorData = true,
+   MAX_TIME = 40;
 var lanes = [
-	{time: 40, led: {red: 17, green: 18}, freetime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */},
-	{time: 40, led: {red: 17, green: 18}, freetime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */},
-	{time: 40, led: {red: 17, green: 18}, freetime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */},
-	{time: 40, led: {red: 17, green: 18}, freetime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */}
+	{time: MAX_TIME, led: {red: 17, green: 18}, freetime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */},
+	{time: MAX_TIME, led: {red: 17, green: 18}, freetime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */},
+	{time: MAX_TIME, led: {red: 17, green: 18}, freetime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */},
+	{time: MAX_TIME, led: {red: 17, green: 18}, freetime: 0, ftItr: 0, state: 0 /* Red default and 1 is for GREEN */}
 	];
 console.log("Initialized the ports sucessfully..."); 
 var signals = {
@@ -29,46 +30,42 @@ while (true) {
 
 	for (var i=0; i<lanes.length; i++) {
 		(lanes[i].state === 0) {
-			signals.on(lanes[i].red);
-			signals.off(lanes[i].green);
+			signals.on(lanes[i].led.red);
+			signals.off(lanes[i].led.green);
 		} else {
 			console.log('green at lane no: ' + (i+1));
-			signals.off(lanes[i].red);
-			signals.on(lanes[i].green);	
+			signals.off(lanes[i].led.red);
+			signals.on(lanes[i].led.green);	
 			lanes[i].ftItr++;	
 			lanenum = i;
 		}
 	}
 		
 	greenTimerExec(greenLane, lanenum);
-	lanes[count].state = 1;
+	lanes[count].state = 0;
 	count++;
 	if (count >= lanes.length) { count = 0;}
 	lanes[count].state = 1;
 }
 
-greenTimerExec function(num) {
+ function greenTimerExec(num) {
 	var lane = lanes[num];
 	var k = lane.time;
 	var sensorCount = 0;
 	while(k>0) {
 		console.log("green signal counter time remaining: " + k);
 		if (!sensorData) { 
-			sensorCount++;
-		} else {
-			sensorCount = 0;
-		}
-		
-		if(sensorCount > 3) {
-			lane.freeTime += sensorCount;
-		}
+			sensorCount++;			
+		} 
 		k--;
 		sleep.usleep(second);
 	}
-	
+	lane.freeTime += sensorCount;
 	if (lane.ftItr >= 4) {
 		var avgTime = (Math.floor(lane.freeTime/(lane.ftItr + 1)));
 		console.log("Average time Set for Lane: " + (num+1) + " Is : " + avgTime + " Seconds.");		
+		avgTime += 5;
+		if (avgTime>= MAX_TIME) {avgTime = MAX_TIME;}
 		lane.time = avgTime;
 	}
 	lanes[num] = lane;
