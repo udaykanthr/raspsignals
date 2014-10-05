3993fc2cd588154b5440497d16f12cc604418b47
urapetix <udaykanthx.rapeti@intel.com>
2014-10-05 06:05:14 +0530

    basic js

--- basic.js
+++ basic.js
@@ -1,12 +1,18 @@
 console.log("This is a nodeJS Application...");
-console.log("Initialized the ports sucessfully..."); 
+console.log("Initializing the ports sucessfully..."); 
 var Gpio = require('onoff').Gpio,
    led = new Gpio(17, 'out'),
    sleep = require('sleep'),
    _baseTime = 128000, //micro seconds
-   sleepTime = _baseTime, 
-   btwCodes = _baseTime * 2,
-   btwLetters = _baseTime * 4,
-   btwWords = _baseTime * 8;
- 
+   sleepTime = _baseTime,
+   btwWords = _baseTime * 8;;
 console.log("Initialized the ports sucessfully..."); 
+
+console.log("Executing the ports..");    
+for (var i=0; i<10; i++) {
+	console.log("setting port17 to ON state");
+	sleep.usleep(btwWords);
+	console.log("setting port17 to OFF state");
+}   
+ 
+console.log("Completed the task."); 
